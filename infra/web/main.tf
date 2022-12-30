f116c76f781bb135bf7fd7f5baa9fc0bebbe917a
Cody Meadows <30964870+cmeadowstech@users.noreply.github.com>
2022-12-30 15:28:18 -0600

    Remove container instance

--- infra/web/main.tf
+++ infra/web/main.tf
@@ -65,35 +65,3 @@ resource "azurerm_cosmosdb_account" "db" {
     failover_priority = 0
   }
 }
-
-resource "azurerm_container_group" "example" {
-  name                = "${var.prefix}-container-${random_integer.ri.result}"
-  location            = data.terraform_remote_state.global.outputs.location
-  resource_group_name = data.terraform_remote_state.global.outputs.rg-name
-  ip_address_type     = "Public"
-  dns_name_label      = "test-${random_integer.ri.result}"
-  os_type             = "Linux"
-
-  container {
-    name   = "hello-world"
-    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 443
-      protocol = "TCP"
-    }
-  }
-
-  container {
-    name   = "sidecar"
-    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
-    cpu    = "0.5"
-    memory = "1.5"
-  }
-
-  tags = {
-    environment = "testing"
-  }
-}
